91d702f6afb00c53de29645e1042a84722d82132
Taemin Hong <taeminhong@outlook.com>
2017-02-23 11:05:11 +0900

    Enable to select bounds source
    You can select where to get corners from; MeshFilter.mesh.bounds or Collider.bounds.

--- Assets/BoundaryPoints/Scripts/LeftBottomPointFinder.cs
+++ Assets/BoundaryPoints/Scripts/LeftBottomPointFinder.cs
@@ -1,16 +1,98 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter))]
 public class LeftBottomPointFinder : MonoBehaviour
 {
-    public GameObject markerPrefab;
-    public CubeCorner WhichCorner = CubeCorner.RightTopFar;
-
-    public MeshFilter target
+    private interface IStrategy
     {
-        get { return GetComponent<MeshFilter>(); }
+        Bounds GetBounds();
+
+        Vector3 ToWorldPosition(Vector3 pos);
+    }
+
+    private abstract class Strategy : IStrategy
+    {
+        private GameObject _gameObject;
+
+        public Strategy(GameObject gameObject)
+        {
+            _gameObject = gameObject;
+        }
+
+        protected T GetComponent<T>()
+        {
+            return _gameObject.GetComponent<T>();
+        }
+
+        protected Transform transform
+        {
+            get { return _gameObject.transform; }
+        }
+
+        public abstract Bounds GetBounds();
+
+        public abstract Vector3 ToWorldPosition(Vector3 pos);
+    }
+
+    private class AccessMesh : Strategy
+    {
+        public AccessMesh(GameObject gameObject) : base(gameObject)
+        {
+        }
+
+        public override Bounds GetBounds()
+        {
+            return GetComponent<MeshFilter>().sharedMesh.bounds;
+        }
+
+        public override Vector3 ToWorldPosition(Vector3 pos)
+        {
+            return transform.localToWorldMatrix.MultiplyPoint(pos);
+        }
+    }
+
+    private class AccessCollider : Strategy
+    {
+        public AccessCollider(GameObject gameObject) : base(gameObject)
+        {
+        }
+
+        public override Bounds GetBounds()
+        {
+            return GetComponent<Collider>().bounds;
+        }
+
+        public override Vector3 ToWorldPosition(Vector3 pos)
+        {
+            return pos;
+        }
+    }
+
+    public enum BoundsSource { Mesh, Collider }
+
+    public GameObject markerPrefab;
+    public CubeCorner corner = CubeCorner.RightTopFar;
+    public BoundsSource boundsSource = BoundsSource.Mesh;
+
+    private IStrategy _strategy;
+
+    private void Awake()
+    {
+        _strategy = SelectStrategy(boundsSource);
+    }
+
+    private IStrategy SelectStrategy(BoundsSource source)
+    {
+        switch (source)
+        {
+            case BoundsSource.Mesh:
+                return new AccessMesh(gameObject);
+
+            default:
+                Debug.Assert(source == BoundsSource.Collider);
+                return new AccessCollider(gameObject);
+        }
     }
 
     private void Start()
@@ -20,15 +102,10 @@ public class LeftBottomPointFinder : MonoBehaviour
 
     private void MarkLeftBottomPoint()
     {
-        var positions = new List<Vector3>();
-        var bounds = target.sharedMesh.bounds;
-        var localToWorld = target.transform.localToWorldMatrix;
-        foreach (var localPos in bounds.GetCorners())
-        {
-            var worldPos = localToWorld.MultiplyPoint(localPos);
-            positions.Add(worldPos);
-        }
-        CreateMarker(positions[(int)WhichCorner]);
+        var bounds = _strategy.GetBounds();
+        var corners = bounds.GetCorners();
+        var i = (int)corner;
+        CreateMarker(_strategy.ToWorldPosition(corners[i]));
     }
 
     private GameObject CreateMarker(Vector3 position)
