7fdb149200655ee6c19e319bc6af748b74699613
Taemin Hong <taeminhong@outlook.com>
2017-02-23 12:21:32 +0900

    Refactoring: rename class

---
+++ Assets/BoundaryPoints/Editor/BoxColliderEncapsulatorEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(BoxColliderEncapsulator))]
+public class BoxColliderEncapsulatorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        if (GUILayout.Button("Encapsulate Children"))
+        {
+            ((BoxColliderEncapsulator)target).Encapsulate();
+        }
+    }
+}
---
+++ Assets/BoundaryPoints/Scripts/BoxColliderEncapsulator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+[ExecuteInEditMode]
+public class BoxColliderEncapsulator : MonoBehaviour
+{
+    public BoxCollider BoxCollider
+    {
+        get { return GetComponent<BoxCollider>(); }
+    }
+
+    public void Encapsulate()
+    {
+        var parentCollider = BoxCollider;
+        var colliders = GetComponentsInChildren<Collider>();
+        var bigBounds = parentCollider.bounds;
+        foreach (var c in colliders)
+        {
+            bigBounds.Encapsulate(c.bounds);
+        }
+        var worldToLocal = transform.worldToLocalMatrix;
+        parentCollider.center = worldToLocal.MultiplyPoint(bigBounds.center);
+        parentCollider.size = worldToLocal.MultiplyVector(bigBounds.size);
+    }
+}
