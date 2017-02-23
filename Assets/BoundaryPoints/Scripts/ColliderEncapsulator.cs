4ac76cc00b0e9e9bafe495868d529e0700a47d33
Taemin Hong <taeminhong@outlook.com>
2017-02-23 12:16:29 +0900

    Introduce ColliderEncapsulator

---
+++ Assets/BoundaryPoints/Scripts/ColliderEncapsulator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+[ExecuteInEditMode]
+public class ColliderEncapsulator : MonoBehaviour
+{
+    public BoxCollider BoxCollider
+    {
+        get { return GetComponent<BoxCollider>(); }
+    }
+}
